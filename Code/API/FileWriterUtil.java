e612b4c6dbb80fa2c27a39e2abc63dc2b30c4ebd
Magna <153079425+Magn4@users.noreply.github.com>
2023-12-13 17:40:57 +0100

    Added a FileWriterUtil, JsonFormatterUtil and API_Test. To try and fetch Informations from the restful API and add them to a file colled Response.json in a JSON Format.

---
+++ Code/API/FileWriterUtil.java
@@ -0,0 +1,15 @@
+package Code.API;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterUtil {
+    public static void writeToFile(String content, String filePath) {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            fileWriter.write(content);
+            System.out.println("Response written to file: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

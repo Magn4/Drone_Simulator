d3626a568af65e825b04f79c4e1c30d6b5ae2595
Magna <153079425+Magn4@users.noreply.github.com>
2023-12-13 19:37:04 +0100

    Removed JsonFormatterUtil

--- Code/API/APIFetcher.java
+++ Code/API/APIFetcher.java
@@ -10,7 +10,7 @@ public class APIFetcher {
     public static void main(String[] args) {
         try {
             final String TOKEN = "Token 6ffe7e815e07b6ede78cade7617454eeb944d168";
-            final URL url = new URL("https://dronesim.facets-labs.com/api/dronedynamics/?format=json");
+            final URL url = new URL("https://dronesim.facets-labs.com/api/drones/?format=json");
 
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
@@ -20,7 +20,6 @@ public class APIFetcher {
             int responseCode = con.getResponseCode();
             System.out.println("Response Code: " + responseCode);
 
-            // Read the response content
             BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
             StringBuilder responseContent = new StringBuilder();
             String line;
@@ -31,16 +30,12 @@ public class APIFetcher {
 
             reader.close();
 
-            // Print the response content
             System.out.println("Response Content: " + responseContent.toString());
 
-            // Specify the full file path where you want the file to be created
             String filePath = "/Users/taha/Desktop/Uni/Java_Project/Drone_Simulator/Code/API/Responses/response.json";
 
-            // Use the FileWriterUtil to write the response content to the specified file
             FileWriterUtil.writeToFile(responseContent.toString(), filePath);
 
-            // Disconnect the connection
             con.disconnect();
 
         } catch (IOException e) {

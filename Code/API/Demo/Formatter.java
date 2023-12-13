ad726accdfe4d8663222d62793033eb0f4f72c18
Magna <153079425+Magn4@users.noreply.github.com>
2023-12-14 00:53:12 +0100

    modified selector

--- Code/API/Demo/Formatter.java
+++ Code/API/Demo/Formatter.java
@@ -1,4 +1,4 @@
-package Code.API;
+package Code.API.Demo;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
--- Code/API/selector.java
+++ Code/API/selector.java
@@ -5,19 +5,5 @@ import com.google.gson.Gson;
 public class Selector {
     public static void main(String[] args){
 
-        String json = """
-                {'name':'Dani',
-                'age': 45,
-                'phone': 222333,
-                'city':'NewYork'}
-                """;
-
-                Gson gson = new Gson();
-
-                User user = gson.fromJson(json, User.class);
-
-                System.out.println(user);
-
-    }
-    
+        
 }

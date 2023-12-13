eed95d2bfa8d66e0195ae8050e58d0d17b72bd3d
Magna <153079425+Magn4@users.noreply.github.com>
2023-12-13 21:34:46 +0100

    Updated pom.xml

--- Code/API/selector.java
+++ Code/API/selector.java
@@ -1,6 +1,23 @@
 package Code.API;
 
+import com.google.gson.Gson;
 
 public class Selector {
+    public static void main(String[] args){
+
+        String json = """
+                {'name':'Dani',
+                'age': 45,
+                'phone': 222333,
+                'city':'NewYork'}
+                """;
+
+                Gson gson = new Gson();
+
+                User user = gson.fromJson(json, User.class);
+
+                System.out.println(user);
+
+    }
     
 }

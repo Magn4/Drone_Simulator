2454900923c1b6843fb3353b0d88a6f6e88b4ca3
NisrineELY <153189620+NisrineELY@users.noreply.github.com>
2024-01-26 17:36:35 +0100

    Update flightDynamics.java

--- src/main/java/GUI/src/MainPages/flightDynamics.java
+++ src/main/java/GUI/src/MainPages/flightDynamics.java
@@ -19,7 +19,7 @@ public class flightDynamics {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Home");
+            JFrame frame = new JFrame("Flight Dynamics");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1500, 1500);
 

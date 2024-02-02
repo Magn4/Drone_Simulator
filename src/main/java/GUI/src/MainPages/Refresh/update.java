28eb8a2b102825f6c11b8962cc2879c0dcea2c0f
NisrineELY <153189620+NisrineELY@users.noreply.github.com>
2024-02-02 21:57:45 +0100

    Create update.java

---
+++ src/main/java/GUI/src/MainPages/Refresh/update.java
@@ -0,0 +1,45 @@
+package GUI.src.MainPages;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class update extends JButton implements ActionListener{
+	
+	JButton update;
+	
+	update(){
+		
+		ImageIcon icon3 = new ImageIcon("src/main/java/GUI/src/Resources/refresh.png");
+		update = new JButton();
+		update.setBounds(1450, 10, 50, 50);
+		update.setIcon(icon3);
+		update.addActionListener(this);
+		update.setBackground(new Color(22, 72, 99));
+        update.setBorderPainted(false); // Setze den Rand des Buttons nicht sichtbar
+
+
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("updated button clicked ! ");
+		flightDynamics.dispose();
+		flightDynamics.main(null);
+		
+		
+	}
+
+	public JButton getupdateButton() {
+		return update;
+	}
+	
+
+	
+	
+
+}
